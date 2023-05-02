a5018080c8478dca62311f3d4544a4bf527e2e03
Herlandro Tribiakto <herlandrotri@gmail.com>
2023-05-02 15:35:59 +0700

    add: new info bro :u

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
+import PrimeVue from 'primevue/config';
 import 'primevue/resources/themes/saga-blue/theme.css'
 import App from './App.vue'
 
-const app = createApp(App).mount('#app');
-
+const app = createApp(App)
 app.use(PrimeVue);
+app.mount('#app');
+
