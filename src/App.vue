9318d9dd52aca1bbc07e5b5ea7e7a052566785ee
Herlandro Tribiakto <herlandrotri@gmail.com>
2023-10-19 14:26:38 +0700

    fix

--- src/App.vue
+++ src/App.vue
@@ -59,10 +59,10 @@ const showSlow = computed(() => {
 });
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-particlesJS.load('particles-js', 'assets/particles.json', function () {
+particlesJS.load('particles-js', '/assets/particles.json', function () {
   console.log('callback - particles.js config loaded');
 });
-particlesJS.load('particles-js2', 'assets/particles_fast.json', function () {
+particlesJS.load('particles-js2', '/assets/particles_fast.json', function () {
   console.log('callback - particles.js config loaded');
 });
 
